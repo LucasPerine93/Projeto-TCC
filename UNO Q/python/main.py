b2a5113095ae28dbed2b2a9844b304ea81460732
Seu Nome <seu.email@example.com>
2026-09-13 19:52:36 -0300

    Otimizações do código foram concluidas (variaveis com o mesmo nome de funções) E fluxo de processamento foi alterado na função  callback_deteccao para quando não for permitido o envio de imagens para o Telegram o MPU não precisar gastar processamento e memoria desnecessariamente processando imagens

--- UNO Q/python/main.py
+++ UNO Q/python/main.py
@@ -37,72 +37,73 @@ class Detectar:
         
     def pessoa_detectada(self, specs_frame, frame=None):
         global matrix
-        global deteccao_ativa
-
-        if ativar_envio == True:
             
-            if frame is None:
-                return
-            try:
-                x1, y1, x2, y2 = specs_frame.get('bounding_box_xyxy') # Pega as cordenadas x e y do retangulo
-                
-                centro_atual = ((x1 + x2) / 2, (y1 + y2) / 2) # Acha o centro do retangulo
-        
-                if self.posicao_anterior is not None:
-                    distancia = math.dist(centro_atual, self.posicao_anterior) # Calcula a distancia do centro atual para a distancia anterior
-                    if distancia < self.limite_movimento: # Se for menor que o limite de movimento não manda a imagem
-                        return
-        
-                img = Image.open(io.BytesIO(frame)).convert('RGB')
-                
-                largura, altura = img.size
-                meio_x = largura // 2
+        if frame is None:
+            return
+        try:
+            x1, y1, x2, y2 = specs_frame.get('bounding_box_xyxy') # Pega as cordenadas x e y do retangulo
+            
+            centro_atual = ((x1 + x2) / 2, (y1 + y2) / 2) # Acha o centro do retangulo
     
-                if self.camera_deteccao == "camera 1":
-                    img_lado = img.crop((0, 0, meio_x, altura)) # Imagem esquerda
-                    cord_retangulo = [x1, y1, x2, y2]
+            if self.posicao_anterior is not None:
+                distancia = math.dist(centro_atual, self.posicao_anterior) # Calcula a distancia do centro atual para a distancia anterior
+                if distancia < self.limite_movimento: # Se for menor que o limite de movimento não manda a imagem
+                    return
     
-                if self.camera_deteccao == "camera 2":
-                    img_lado = img.crop((meio_x, 0, largura, altura)) # Imagem direita
-                    cord_retangulo = [x1 - meio_x, y1, x2 - meio_x, y2]
+            img = Image.open(io.BytesIO(frame)).convert('RGB')
+            
+            largura, altura = img.size
+            meio_x = largura // 2
+
+            if self.camera_deteccao == "camera 1":
+                img_lado = img.crop((0, 0, meio_x, altura)) # Imagem esquerda
+                cord_retangulo = [x1, y1, x2, y2]
+
+            if self.camera_deteccao == "camera 2":
+                img_lado = img.crop((meio_x, 0, largura, altura)) # Imagem direita
+                cord_retangulo = [x1 - meio_x, y1, x2 - meio_x, y2]
+            
+            ImageDraw.Draw(img_lado).rectangle(cord_retangulo, outline="red", width=3)
+    
+            bytes_imagem_com_caixas = io.BytesIO()
+            img_lado.save(bytes_imagem_com_caixas, format="JPEG")
+            imagem_com_caixas = bytes_imagem_com_caixas.getvalue()
                 
-                ImageDraw.Draw(img_lado).rectangle(cord_retangulo, outline="red", width=3)
-        
-                bytes_imagem_com_caixas = io.BytesIO()
-                img_lado.save(bytes_imagem_com_caixas, format="JPEG")
-                imagem_com_caixas = bytes_imagem_com_caixas.getvalue()
-                    
-                bot.send_photo(ID_chat, imagem_com_caixas, f"Pessoa detectada na {self.camera_deteccao}")
-        
-                if matrix == True: 
-                    Bridge.call("pessoaDetectada")
-        
-                self.posicao_anterior = centro_atual
-                
-            except Exception as e:
-                print(f"[ERRO]: {e}")
+            bot.send_photo(ID_chat, imagem_com_caixas, f"Pessoa detectada na {self.camera_deteccao}")
+    
+            if matrix == True: 
+                Bridge.call("pessoaDetectada")
+    
+            self.posicao_anterior = centro_atual
+            
+        except Exception as e:
+            print(f"[ERRO]: {e}")
 
 espiao1 = Detectar(camera_deteccao="camera 1", limite_movimento=55)
 espiao2 = Detectar(camera_deteccao="camera 2", limite_movimento=40)
 
 def callback_deteccao(specs_frame, frame=None):
-    if frame is None:
-        return
-        
-    img_tamanho = Image.open(io.BytesIO(frame))
-    largura, _ = img_tamanho.size
-
-    img_metade = largura // 2
+    global ativar_envio
     
-    x1, y2, x2, y2 = specs_frame.get("bounding_box_xyxy")
-    centro = ((x1 + x2) / 2)
+    if ativar_envio == True:
+        
+        if frame is None:
+            return
+            
+        img_tamanho = Image.open(io.BytesIO(frame))
+        largura, _ = img_tamanho.size
 
-    if centro is not None:
-        if centro < img_metade:
-            espiao1.pessoa_detectada(specs_frame, frame)
+        img_metade = largura // 2
+        
+        x1, y2, x2, y2 = specs_frame.get("bounding_box_xyxy")
+        centro = ((x1 + x2) / 2)
 
-        else:
-            espiao2.pessoa_detectada(specs_frame, frame)
+        if centro is not None:
+            if centro < img_metade:
+                espiao1.pessoa_detectada(specs_frame, frame)
+
+            else:
+                espiao2.pessoa_detectada(specs_frame, frame)
             
 
 def esconder_matrix(Sender, _):
@@ -142,13 +143,13 @@ def desbloquear_servidor(Sender, _):
     
     Sender.reply("Sevidor desbloqueado")
 
-def ativar_envio(Sender, _):
+def envio_ativado(Sender, _):
     global ativar_envio
     ativar_envio = True
 
     Sender.reply("O envio de images foi ativado")
     
-def desativar_envio(Sender, _):
+def envio_desativado(Sender, _):
     global ativar_envio
     ativar_envio = False
 
@@ -201,7 +202,7 @@ bot.add_command("bloquear_servidor", bloquear_servidor)
 bot.add_command("desbloquear_servidor", desbloquear_servidor)
 bot.add_command("comandos", mostrar_comandos)
 bot.add_command("dispositivos", apr_conectados)
-bot.add_command("ativar_envio", ativar_envio)
-bot.add_command("desativar_envio", desativar_envio)
+bot.add_command("ativar_envio", envio_ativado)
+bot.add_command("desativar_envio", envio_desativado)
 
 App.run()
